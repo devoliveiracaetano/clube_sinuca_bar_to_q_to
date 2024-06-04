ad50afe4a1679c4b69381233345027621a04124d
Altamir Caetano de Oliveira <altcaeol@gmail.com>
2024-06-04 11:32:25 -0300

    Criacao Projeto Loja Virtual Clube Sinuca

---
+++ loja_virtual_clube_sinuca/src/main/java/com/altsystem/LojaVirtualClubeSinucaApplication.java
@@ -0,0 +1,13 @@
+package com.altsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualClubeSinucaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualClubeSinucaApplication.class, args);
+	}
+
+}
